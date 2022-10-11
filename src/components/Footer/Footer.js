9385ae4833321ae9ed2d9bd16f60e5cff8076d08
raju-miah <rajumiahpollob7@gmail.com>
2022-10-11 17:47:34 +0600

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <p><small>Copyright || 2022 by @team Web Development Quiz</small></p>
+        </div>
+    );
+};
+
+export default Footer;
