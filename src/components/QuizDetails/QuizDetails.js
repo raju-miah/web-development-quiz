643465ec19759bb3d18492a25c275cf203daef13
raju-miah <rajumiahpollob7@gmail.com>
2022-10-12 12:53:05 +0600

    show topic name in quiz

--- src/components/QuizDetails/QuizDetails.js
+++ src/components/QuizDetails/QuizDetails.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import QuizQuestion from '../QuizQuestion/QuizQuestion';
+import './QuizDetails.css';
 
 const QuizDetails = () => {
     const quizDetails = useLoaderData().data;
     // console.log(quizDetails)
-    const { questions } = quizDetails;
+    const { name, questions } = quizDetails;
     // console.log(questions)
 
     return (
         <div>
-            <h1>WelCome to Quiz</h1>
+            <h1>WelCome to Quiz of</h1>
+            <h2 className='quiz-topic-name'>{name}</h2>
             <h3>Start your IQ here</h3>
             {
                 questions.map(quizQuestion => <QuizQuestion
