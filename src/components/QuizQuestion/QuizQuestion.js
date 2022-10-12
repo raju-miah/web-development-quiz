b793c4762dea1f51e02c8990e9b7f94ae8991b89
raju-miah <rajumiahpollob7@gmail.com>
2022-10-12 12:32:52 +0600

    added btn correct wrong toast

--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -7,13 +7,27 @@ import './QuizQuestion.css';
 
 const QuizQuestion = ({ quizQuestion }) => {
     // console.log(quizQuestion)
-    const { id, question, options, correctAnswer } = quizQuestion;
-    console.log(id, correctAnswer)
+    const { question, options, correctAnswer } = quizQuestion;
+    // console.log(id, correctAnswer)
+
+    const handelCorrectAndWrong = (option) => {
+        // console.log(option)
+        if (option === correctAnswer) {
+            toast.success('CorrectAnswer')
+
+        }
+        else {
+            toast.error('WrongAnswer')
+        }
+        // console.log(correctAnswer)
+    }
 
 
     const notify = () => {
         toast.success(`${correctAnswer}`)
     }
+
+
     return (
         <div className='quiz'>
             <h1>{question}</h1>
@@ -22,10 +36,10 @@ const QuizQuestion = ({ quizQuestion }) => {
             </button>
 
             <div className='options'>
-                <button>{options[0]}</button>
-                <button>{options[1]}</button>
-                <button>{options[2]}</button>
-                <button>{options[3]}</button>
+                <button onClick={() => handelCorrectAndWrong(options[0])}>{options[0]}</button>
+                <button onClick={() => handelCorrectAndWrong(options[1])}>{options[1]}</button>
+                <button onClick={() => handelCorrectAndWrong(options[2])}>{options[2]}</button>
+                <button onClick={() => handelCorrectAndWrong(options[3])}>{options[3]}</button>
             </div>
             <Toaster
                 position="top-center"
